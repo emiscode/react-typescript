b58b7dd8ba332e9ed6998511eba4d414dcf1d229
Emilio Scodeler <emilios@ciandt.com>
2022-06-12 01:33:13 -0300

    Using state and adding onchange behavior

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,9 +3,19 @@ import Button from '../Button';
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+    state = {
+        task: "",
+        time: "00:00"
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <form className={style.addTask}>
+            <form className={style.addTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='task'> Add Study </label>
                     <input
@@ -13,6 +23,8 @@ class Form extends React.Component {
                         name='task'
                         id='task'
                         placeholder='What do you want to study?'
+                        value={this.state.task}
+                        onChange={event => this.setState({...this.state, task:event.target.value})}
                         required
                     />
                 </div>
@@ -23,6 +35,8 @@ class Form extends React.Component {
                         name='time'
                         id='time'
                         step='1'
+                        value={this.state.time}
+                        onChange={event => this.setState({...this.state, time: event.target.value })}
                         min='00:00:00'
                         max='01:30:00'
                         required
