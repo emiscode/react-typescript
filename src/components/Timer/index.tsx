e1a37e50157ab6f102ff8c493c3c43e25a2f3624
Emilio Scodeler <emilios@ciandt.com>
2022-06-12 16:09:18 -0300

    Add useEffect to get the selected task time

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,10 +1,23 @@
-import { timeToSeconds } from "../../common/utils/date";
+import { useEffect, useState } from "react";
+import { timeToSeconds } from "../../common/utils/timeConverter";
+import { ITask } from "../../types/ITask";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
-    console.log(timeToSeconds('01:01:01'));
+interface Props {
+    selected: ITask | undefined
+}
+
+export default function Timer({ selected }: Props) {
+    const [time, setTime] = useState<number>();
+
+    useEffect(() => {
+        if (selected?.time) {
+            setTime(timeToSeconds(selected?.time));
+        }
+    }, [selected]);
+
     return (
         <div className={style.timer}>
             <p className={style.title}>Pick up a card and start the timer!</p>
