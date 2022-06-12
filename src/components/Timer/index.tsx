956d2a61f6f1b7b5724719459af8a8804b55749b
Emilio Scodeler <emilios@ciandt.com>
2022-06-12 15:40:27 -0300

    Add function to convert time to seconds

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,8 +1,10 @@
+import { timeToSeconds } from "../../common/utils/date";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
 export default function Timer() {
+    console.log(timeToSeconds('01:01:01'));
     return (
         <div className={style.timer}>
             <p className={style.title}>Pick up a card and start the timer!</p>
