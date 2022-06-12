600892615f67ae8e07a4a36a1e2ff3fa3e36aa63
Emilio Scodeler <emilios@ciandt.com>
2022-06-12 01:11:43 -0300

    Test using state

--- src/components/ListComponent/index.tsx
+++ src/components/ListComponent/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
-    const tasks = [
+    const [tasks, setTasks] = useState([
         {
             task: 'React',
             time: '02:00:00'
@@ -11,11 +12,13 @@ function List() {
             task: 'TypeScript',
             time: '03:00:00'
         }
-    ];
+    ]);
 
     return (
         <aside className={style.listTasks}>
-            <h2 className={style.title}> Daily Studies </h2>
+            <h2 className={style.title} onClick={() => {
+                setTasks([...tasks, { task: "Hooks", time: "05:00:00"}]);
+            }}> Daily Studies </h2>
             <ul>
                {tasks.map((item, index) => (
                    <Item 
